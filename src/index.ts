dc2aa4b4fe225e9fd981133c5750f15ba4b4d888
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-07-25 15:21:02 +0200

    Add final version

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,6 @@ interface User {
   age: number;
 }
 
-const startApp = async () => {
-
-
   class Message {
     private content: string;
 
@@ -80,6 +77,7 @@ const startApp = async () => {
         Message.showColorized(MessageVariant.Success, 'User has been successfully added')
       }
     }
+
     remove(userName: string){
       const index = this.data.findIndex((user) => user.name === userName);
 
@@ -90,19 +88,55 @@ const startApp = async () => {
         Message.showColorized(MessageVariant.Error, 'User not found...')
       }
     }
-
   }
 
-inquirer.prompt([{
-  name: 'action',
-  type: 'input',
-  message: 'How can I help you?',
-}]).then((answers: InquirerAnswers) => {
-  console.log("Chosen action: " + answers.action);
-  startApp();
-  if (answers.action === "quit")
-    return;
-});
-}
+  const users = new UsersData();
+  console.log("\n");
+  console.info("???? Welcome to the UsersApp!");
+  console.log("====================================");
+  Message.showColorized(MessageVariant.Info, "Available actions");
+  console.log("\n");
+  console.log("list – show all users");
+  console.log("add – add new user to the list");
+  console.log("remove – remove user from the list");
+  console.log("quit – quit the app");
+  console.log("\n");
 
-startApp();
+  const startApp = () => {
+    inquirer.prompt([{
+      name: 'action',
+      type: 'input',
+      message: 'How can I help you?',
+    }]).then(async (answers: InquirerAnswers) => {
+      switch (answers.action) {
+        case Action.List:
+          users.showAll();
+          break;
+        case Action.Add:
+          const user = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }, {
+            name: 'age',
+            type: 'number',
+            message: 'Enter age',
+          }]);
+          users.add(user);
+          break;
+        case Action.Remove:
+          const name = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }]);
+          users.remove(name.name);
+          break;
+        case Action.Quit:
+          Message.showColorized(MessageVariant.Info, "Bye bye!");
+          return;
+      }
+
+      startApp();
+    });
+  }
